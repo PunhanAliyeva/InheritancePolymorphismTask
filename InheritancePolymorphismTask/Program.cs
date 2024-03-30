16f1317b63b962e09d081643731bc14918e61266
PunhanAliyeva <eliyevapunhan567@gmail.com>
2024-03-31 01:30:24 +0400

    Add project files.

---
+++ InheritancePolymorphismTask/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace InheritancePolymorphismTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student(2,12,4,5,23,78,67);
+            foreach (var grade in student.Grades)
+            {
+                Console.WriteLine(grade);
+            }
+            
+        }
+    }
+}
---
+++ InheritancePolymorphismTask/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritancePolymorphismTask
+{
+    internal class Student
+    {
+        public int[] Grades { get; set; }
+
+        public Student(params int[] grades)    //1,2,3,4,5,6,7
+        {
+            int count = 0;
+            foreach (var grade in grades)
+            {
+                if (grade % 2 == 0)
+                {
+                    count++;
+                }
+            }
+            Grades = new int[count];
+            int index = 0;
+            foreach (var evenGrade in grades)
+            {
+                if(evenGrade % 2 == 0)
+                {
+                    Grades[index++] = evenGrade;
+                }
+            }
+        }
+
+    }
+}
